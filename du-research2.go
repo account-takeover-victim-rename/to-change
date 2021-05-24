cb0325d0bc0cae05a04911644db0520df344c3b7
du-research2 <you@example.com>
2021-05-24 14:46:44 +0000

    first commit

---
+++ du-research2.go
@@ -0,0 +1,20 @@
+package tochange
+
+import "github.com/ventu-io/go-shortid"
+import "fmt"
+
+
+func Shalom(name string) string {
+    // Return a greeting that embeds the name in a message.
+    fmt.Println(shortid.Generate())
+    message := fmt.Sprintf("Shalom Shalom, Im Bad, %v. Welcome!", name)
+    return message
+}
+
+
+//func main() {
+    // Get a greeting message and print it.
+//    fmt.Println("Your short ID is:")
+//    fmt.Println(shortid.Generate())
+//}
+
